09a26b8f2e04b7f9c8f76dc2e4ac983f328ae824
isun3071 <isun@bu.edu>
2026-04-17 16:06:44 -0400

    ipynb in

--- randomsample.py
+++ randomsample.py
@@ -1,14 +1,25 @@
-with open('bu_org_emails.txt', 'r') as f:
-    orgs = f.read().strip().split('\n')
+import random
 
-def random_sample_org():
-    import random
+def random_sample_org(file='remaining_orgs.txt'):
+    with open(file, 'r') as f:
+        orgs = f.read().strip().split('\n')
+    print(len(orgs))
     x = []
-    for i in range(20):
+    for i in range(10):
         x.append(orgs.pop(random.randint(0, len(orgs) - 1)))
+    print(len(orgs))
+    with open(file, 'w') as f:
+        f.write('\n'.join(orgs))
     return x
+    
 
-print(random_sample_org())
+y = random_sample_org()
+print(y)
 with open('random_sample_orgs.txt', 'w') as f:
-    for org in random_sample_org():
+    for org in y:
+        f.write(org + '\n')
+
+
+with open('sent_orgs.txt', 'a') as f:
+    for org in y:
         f.write(org + '\n')
